fc7fb688bbbd64bee6f106edd4f190cd568f00d7
Andres <andres.gonzalez2392@gmail.com>
2022-08-26 16:26:23 -0400

    Initial Commit

---
+++ Developers/DevelopersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopersApp.swift
+//  Developers
+//
+//  Created by PDWS on 8/26/22.
+//
+
+import SwiftUI
+
+@main
+struct DevelopersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
